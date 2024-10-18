225b6615a0f983a4a7dee0a5a8797f7aabb4a30b
Thien-Hong N <tnguyen.27@presentationhs.org>
2024-10-17 18:28:19 -0700

    Filled in the methods in ExampleSmartMotorController

--- PID_Example_v3/src/main/java/frc/robot/ExampleSmartMotorController.java
+++ PID_Example_v3/src/main/java/frc/robot/ExampleSmartMotorController.java
@@ -4,12 +4,32 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 /**
  * A simplified stub class that simulates the API of a common "smart" motor controller.
  *
  * <p>Has no actual functionality.
  */
 public class ExampleSmartMotorController {
+
+  private final WPI_TalonSRX m_motor = new WPI_TalonSRX(5);
+  private double m_kp;
+  private double m_ki;
+  private double m_kd;
+
+  private static final double kEncoderCPR = 4096;
+
+  private double rotationsToCounts(double rotation) {
+    return rotation * kEncoderCPR;
+  }
+
+  private double countsToRotations(double encoderCounts) {
+    return encoderCounts / kEncoderCPR;
+  }
+
   public enum PIDMode {
     kPosition,
     kVelocity,
@@ -22,7 +42,14 @@ public class ExampleSmartMotorController {
    * @param port The port for the controller.
    */
   @SuppressWarnings("PMD.UnusedFormalParameter")
-  public ExampleSmartMotorController(int port) {}
+  public ExampleSmartMotorController(int port) {
+    m_motor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+    m_motor.selectProfileSlot(0, 0);
+
+    m_motor.config_kP(0, m_kp);
+    m_motor.config_kI(0, m_ki);
+    m_motor.config_kD(0, m_kd);
+  }
 
   /**
    * Example method for setting the PID gains of the smart controller.
@@ -31,7 +58,11 @@ public class ExampleSmartMotorController {
    * @param ki The integral gain.
    * @param kd The derivative gain.
    */
-  public void setPID(double kp, double ki, double kd) {}
+  public void setPID(double kp, double ki, double kd) {
+    m_kp = kp;
+	  m_ki = ki;
+	  m_kd = kd;
+  }
 
   /**
    * Example method for setting the setpoint of the smart controller in PID mode.
@@ -40,7 +71,27 @@ public class ExampleSmartMotorController {
    * @param setpoint The controller setpoint.
    * @param arbFeedforward An arbitrary feedforward output (from -1 to 1).
    */
-  public void setSetpoint(PIDMode mode, double setpoint, double arbFeedforward) {}
+  public void setSetpoint(PIDMode mode, double setpoint, double arbFeedforward) {
+    ControlMode controlMode;
+
+    switch (mode) {
+      default :
+      case kPosition :
+        controlMode = ControlMode.Position;
+        break;
+
+      case kVelocity :
+        controlMode = ControlMode.Velocity;
+        setpoint /= 10;
+        break;
+
+      case kMovementWitchcraft : // is not used in this code
+        controlMode = ControlMode.MotionMagic;
+        break;
+    }
+
+    m_motor.set(controlMode, rotationsToCounts(setpoint));
+  }
 
   /**
    * Places this motor controller in follower mode.
@@ -55,7 +106,7 @@ public class ExampleSmartMotorController {
    * @return The current encoder distance.
    */
   public double getEncoderDistance() {
-    return 0;
+    return countsToRotations(m_motor.getSelectedSensorPosition(0));
   }
 
   /**
@@ -64,25 +115,35 @@ public class ExampleSmartMotorController {
    * @return The current encoder rate.
    */
   public double getEncoderRate() {
-    return 0;
+    return countsToRotations(m_motor.getSelectedSensorVelocity(0) * 10);
   }
 
   /** Resets the encoder to zero distance. */
-  public void resetEncoder() {}
+  public void resetEncoder() {
+    m_motor.setSelectedSensorPosition(0, 0, 0);
+  }
 
-  public void set(double speed) {}
+  public void set(double voltage) {
+    m_motor.set(ControlMode.PercentOutput, voltage);
+  }
 
   public double get() {
-    return 0;
+    return m_motor.getMotorOutputPercent();
   }
 
-  public void setInverted(boolean isInverted) {}
+  public void setInverted(boolean isInverted) {
+    m_motor.setInverted(isInverted);
+  }
 
   public boolean getInverted() {
-    return false;
+    return m_motor.getInverted( );
   }
 
-  public void disable() {}
+  public void disable() {
+    m_motor.set(ControlMode.Disabled, 0);
+  }
 
-  public void stopMotor() {}
+  public void stopMotor() {
+    set(0.0);
+  }
 }
--- PID_Example_v3/src/main/java/frc/robot/Robot.java
+++ PID_Example_v3/src/main/java/frc/robot/Robot.java
@@ -6,13 +6,14 @@ package frc.robot;
 
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
   private static double kDt = 0.02;
 
-  private final Joystick m_joystick = new Joystick(1);
+  private final XboxController m_controller = new XboxController(0);
   private final ExampleSmartMotorController m_motor = new ExampleSmartMotorController(1);
   // Note: These gains are fake, and will have to be tuned for your robot.
   private final SimpleMotorFeedforward m_feedforward = new SimpleMotorFeedforward(1, 1.5);
@@ -27,11 +28,12 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     // Note: These gains are fake, and will have to be tuned for your robot.
-    m_motor.setPID(1.3, 0.0, 0.7);
+    m_motor.setPID(0.5, 0.0, 0.0);
   }
 
   @Override
   public void teleopPeriodic() {
+  /*
     if (m_joystick.getRawButtonPressed(2)) {
       m_goal = new TrapezoidProfile.State(5, 0);
     } else if (m_joystick.getRawButtonPressed(3)) {
@@ -47,5 +49,16 @@ public class Robot extends TimedRobot {
         ExampleSmartMotorController.PIDMode.kPosition,
         m_setpoint.position,
         m_feedforward.calculate(m_setpoint.velocity) / 12.0);
+    */
+
+    SmartDashboard.putNumber("Elevator Rotations", m_motor.getEncoderDistance());
+
+    while (m_controller.getAButtonPressed()) {
+      m_motor.set(0.3);
+    }
+
+    while (m_controller.getBButtonPressed()) {
+      m_motor.set(-0.3);
+    }
   }
 }
