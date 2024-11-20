ca2ca737ce404b6584388ecbb826c66503b9d27e
Jeff Mullins <jeff.l.mullins@gmail.com>
2024-11-20 12:19:43 -0500

    Added auto-formatter and cleanup comments

--- tnguyen/ElevatorPID_THN/src/main/java/frc/robot/ExampleSmartMotorController.java
+++ tnguyen/ElevatorPID_THN/src/main/java/frc/robot/ExampleSmartMotorController.java
@@ -14,52 +14,50 @@ import edu.wpi.first.wpilibj.DataLogManager;
 /**
  * A simplified stub class that simulates the API of a common "smart" motor controller.
  *
- * <p>Has no actual functionality.
+ * <p>
+ * Has no actual functionality.
  */
-public class ExampleSmartMotorController {
-
-  private final WPI_TalonSRX m_motor = new WPI_TalonSRX(5);
-  private double m_kp;
-  private double m_ki;
-  private double m_kd;
-
-  private static double m_kEncoderCPR;
-
-public ExampleSmartMotorController(){};
-
-  private TalonSRXSimCollection m_motorSim;
-  /**
-   * COUNTS TO ROTATIONS
-   * encoder reads values in counts, but human users input rotations for simplicity
-   * 4096 counts in one rotation, following methods converts counts to rotations, vice versa
-   */
-
-  private double rotationsToCounts(double rotation) { 
-    return rotation * m_kEncoderCPR;
-  }
-
-  private double countsToRotations(double encoderCounts) {
-    return encoderCounts / m_kEncoderCPR;
-  }
+public class ExampleSmartMotorController
+{
+  // TODO: Keep your constants and variables at the top of the class--it's where people look for them
+  //      I moved PIDMode and m_motorSim up here
 
   /**
    * declares PIDMode as an enum
    * PIDMode used to determine controlmode
    */
-  public enum PIDMode {
-    kPosition,
-    kVelocity,
-    kMovementWitchcraft
+  public enum PIDMode
+  {
+    kPosition, kVelocity, kMovementWitchcraft
   }
+
+  // TODO:  Since we don't want to have this configure a specific motor controller (5) as a default
+  //        the "port" definition coming in will initialize a WPI_TalonSRX with the correct port
+  //        also, "final" means this m_motor cannot be changed (ever), so just declare an m_motor
+  //        variable without "final" and without the initialization "= new WPI_TalonSRX(5)"
+  private final WPI_TalonSRX    m_motor = new WPI_TalonSRX(5);
+  private TalonSRXSimCollection m_motorSim;
+
+  private double                m_kp;
+  private double                m_ki;
+  private double                m_kd;
+
+  private static double         m_kEncoderCPR;  // TODO: Since this is no longer a constant (within this class), remove the "k"
+  // And the "static" is probably not needed (although won't cause an error)
+
+  // TODO: Keep your constructors at the top of the class--it's where everyone will look for them
+  //      I moved yours here
 
   /**
    * Creates a new ExampleSmartMotorController.
    *
-   * @param port The port for the controller.
+   * @param port
+   *          The port for the controller.
    */
   @SuppressWarnings("PMD.UnusedFormalParameter")
-  public ExampleSmartMotorController(int ports, double kEncoderCPR) {
-    m_motor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder); 
+  public ExampleSmartMotorController(int ports, double kEncoderCPR)
+  {
+    m_motor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
     //encoder type in TalonSRX is quadrature encoder
     m_motor.selectProfileSlot(0, 0);
 
@@ -69,38 +67,69 @@ public ExampleSmartMotorController(){};
     m_kEncoderCPR = kEncoderCPR;
   }
 
+  // TODO: Our new usage now requires a port and encoder parameter
+  //        delete this now unused constructor interface below
+  public ExampleSmartMotorController( )
+  {};
+
+  /**
+   * COUNTS TO ROTATIONS
+   * encoder reads values in counts, but human users input rotations for simplicity
+   * 4096 counts in one rotation, following methods converts counts to rotations, vice versa
+   */
+
+  private double rotationsToCounts(double rotations)
+  {
+    return rotations * m_kEncoderCPR;
+  }
+
+  private double countsToRotations(double encoderCounts)
+  {
+    return encoderCounts / m_kEncoderCPR;
+  }
 
   /**
    * Example method for setting the PID gains of the smart controller.
    *
-   * @param kp The proportional gain.
-   * @param ki The integral gain.
-   * @param kd The derivative gain.
+   * @param kp
+   *          The proportional gain.
+   * @param ki
+   *          The integral gain.
+   * @param kd
+   *          The derivative gain.
    */
-  public void setPID(double kp, double ki, double kd) {
+  public void setPID(double kp, double ki, double kd)
+  {
     m_kp = kp;
     m_ki = ki;
     m_kd = kd;
     m_motor.config_kP(0, kp);
     m_motor.config_kI(0, ki);
     m_motor.config_kD(0, kd);
+    // TODO: It would be nice to log when the PID is changed, yes?
   }
 
-  public double getKp() {
-    return m_kp; 
+  public double getKp( )
+  {
+    return m_kp;
   }
 
   /**
    * Example method for setting the setpoint of the smart controller in PID mode.
    *
-   * @param mode The mode of the PID controller.
-   * @param setpoint The controller setpoint.
-   * @param arbFeedforward An arbitrary feedforward output (from -1 to 1).
+   * @param mode
+   *          The mode of the PID controller.
+   * @param setpoint
+   *          The controller setpoint.
+   * @param arbFeedforward
+   *          An arbitrary feedforward output (from -1 to 1).
    */
-  public void setSetpoint(PIDMode mode, double setpoint, double arbFeedforward) {
+  public void setSetpoint(PIDMode mode, double setpoint, double arbFeedforward)
+  {
     ControlMode controlMode;
 
-    switch (mode) {
+    switch (mode)
+    {
       default :
       case kPosition :
         controlMode = ControlMode.Position;
@@ -117,23 +146,30 @@ public ExampleSmartMotorController(){};
     }
 
     m_motor.set(controlMode, rotationsToCounts(setpoint));
-    DataLogManager.log("ControlMode: " + controlMode);
-    DataLogManager.log("Setpoint: " + rotationsToCounts(setpoint));
-}
+    // TODO: You'll find that when you log stuff on separate lines, it increases the "spam" factor to the console
+    //        Put this on one line. This would also make it easy to cut and paste in to a spreadsheet for plotting if needed.
+    //        And just display the passed in setpoint, not the value going to the motors
+    DataLogManager.log("ControlMode: " + controlMode + " Setpoint: " + setpoint);
+    // DataLogManager.log("ControlMode: " + controlMode);
+    // DataLogManager.log("Setpoint: " + rotationsToCounts(setpoint));
+  }
 
   /**
    * Places this motor controller in follower mode.
    *
-   * @param leader The leader to follow.
+   * @param leader
+   *          The leader to follow.
    */
-  public void follow(ExampleSmartMotorController leader) {}
+  public void follow(ExampleSmartMotorController leader)
+  {}
 
   /**
    * Returns the encoder distance.
    *
    * @return The current encoder distance.
    */
-  public double getEncoderDistance() {
+  public double getEncoderDistance( )
+  {
     return countsToRotations(m_motor.getSelectedSensorPosition(0));
   }
 
@@ -142,59 +178,74 @@ public ExampleSmartMotorController(){};
    *
    * @return The current encoder rate.
    */
-  public double getEncoderRate() {
+  public double getEncoderRate( )
+  {
     return countsToRotations(m_motor.getSelectedSensorVelocity(0) * 10);
   }
 
   /** Resets the encoder to zero distance. */
-  public void resetEncoder() {
+  public void resetEncoder( )
+  {
+    // TODO: It would be nice to log when the encoder is reset, yes?
     m_motor.setSelectedSensorPosition(0, 0, 0);
   }
 
   /**
    * used to set the constant speed of the motor, in percentOutput
    */
-  public void set(double voltage) { // set the speed of the motor using percent output
+  public void set(double voltage)
+  { // set the speed of the motor using percent output
+   // TODO: It would be nice to log when the motor speed is changed, yes?
     m_motor.set(ControlMode.PercentOutput, voltage);
   }
 
-  public double get() {
-    return m_motor.getMotorOutputPercent();
+  public double get( )
+  {
+    return m_motor.getMotorOutputPercent( );
   }
 
   /** Inverts motor direction */
-  public void setInverted(boolean isInverted) {
+  public void setInverted(boolean isInverted)
+  {
+    // TODO: It would be nice to log when the inversion is changed, yes?
     m_motor.setInverted(isInverted);
   }
 
-  public boolean getInverted() {
-    return m_motor.getInverted();
+  public boolean getInverted( )
+  {
+    return m_motor.getInverted( );
   }
 
-  public double getClosedLoopError() {
-    return (m_motor.getClosedLoopError());
+  public double getClosedLoopError( )
+  {
+    return (m_motor.getClosedLoopError( ));
   }
 
-  public void disable() {
+  public void disable( )
+  {
+    // TODO: It would be nice to log when the motor is disabled, yes?
     m_motor.set(ControlMode.Disabled, 0);
   }
 
-  public double getVelocity() {
-    return m_motor.getSelectedSensorVelocity();
+  public double getVelocity( )
+  {
+    return m_motor.getSelectedSensorVelocity( );
   }
 
-  public void stopMotor() {
+  public void stopMotor( )
+  {
+    // TODO: It would be nice to log when the motor is stopped, yes?
     set(0.0);
   }
 
   /**
-  * Return the motor simulation object for this controller
-  *
-  * @return motor simulation object
-  */
- public TalonSRXSimCollection getMotorSimulation( )
- {
-   return m_motorSim;
- }
+   * Return the motor simulation object for this controller
+   *
+   * @return motor simulation object
+   */
+  public TalonSRXSimCollection getMotorSimulation( )
+  {
+    return m_motorSim;
+  }
 
 }
--- tnguyen/ElevatorPID_THN/src/main/java/frc/robot/Robot.java
+++ tnguyen/ElevatorPID_THN/src/main/java/frc/robot/Robot.java
@@ -13,112 +13,129 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class Robot extends TimedRobot {
- private static double kDt = 0.02;
- private double goal;
- private final static double kEncoderCPR = 4096;
-//  private final ExampleSmartMotorController m_ExampleSmartMotorController = new ExampleSmartMotorController();
+public class Robot extends TimedRobot
+{
+  private static double                     kDt           = 0.02;
+  private double                            goal;
+  private final static double               kEncoderCPR   = 4096;
+  //  private final ExampleSmartMotorController m_ExampleSmartMotorController = new ExampleSmartMotorController();
 
- private final XboxController m_controller = new XboxController(0);
- private final ExampleSmartMotorController m_motor = new ExampleSmartMotorController(5, kEncoderCPR);
- private final SimpleMotorFeedforward m_feedforward = new SimpleMotorFeedforward(1, 1.5);
- private final TrapezoidProfile m_profile =
-     new TrapezoidProfile(new TrapezoidProfile.Constraints(1.0, 2.0));
- private TrapezoidProfile.State m_goal = new TrapezoidProfile.State();
- private TrapezoidProfile.State m_setpoint = new TrapezoidProfile.State();
+  private final XboxController              m_controller  = new XboxController(0);
+  private final ExampleSmartMotorController m_motor       = new ExampleSmartMotorController(5, kEncoderCPR);
+  private final SimpleMotorFeedforward      m_feedforward = new SimpleMotorFeedforward(1, 1.5);
+  private final TrapezoidProfile            m_profile     = new TrapezoidProfile(new TrapezoidProfile.Constraints(1.0, 1.0));
+  private TrapezoidProfile.State            m_goal        = new TrapezoidProfile.State( );
+  private TrapezoidProfile.State            m_setpoint    = new TrapezoidProfile.State( );
 
- private final TalonSRXSimCollection m_motorSim = m_motor.getMotorSimulation();
- private final ElevSim m_elevSim = new ElevSim(m_motorSim, kEncoderCPR);
+  private final TalonSRXSimCollection       m_motorSim    = m_motor.getMotorSimulation( );
+  private final ElevSim                     m_elevSim     = new ElevSim(m_motorSim, kEncoderCPR);
 
- 
+  @Override
+  public void robotInit( )
+  {
+    // Note: These gains are fake, and will have to be tuned for your robot.
+    m_motor.setPID(0.27, 0.0, 0.0);
+    m_motor.resetEncoder( );
+    m_elevSim.periodic( );
+    DataLogManager.start( );
+  }
 
- @Override
- public void robotInit() {
-   // Note: These gains are fake, and will have to be tuned for your robot.
-   m_motor.setPID(0.27, 0.0, 0.0);
-   m_motor.resetEncoder();
-   m_elevSim.periodic( );
-   DataLogManager.start();
- }
+  @Override
+  public void teleopPeriodic( )
+  {
+    /*
+     * if (m_joystick.getRawButtonPressed(2)) {
+     * m_goal = new TrapezoidProfile.State(5, 0);
+     * } else if (m_joystick.getRawButtonPressed(3)) {
+     * m_goal = new TrapezoidProfile.State();
+     * }
+     * 
+     * // Retrieve the profiled setpoint for the next timestep. This setpoint moves
+     * // toward the goal while obeying the constraints.
+     * m_setpoint = m_profile.calculate(kDt, m_setpoint, m_goal);
+     * 
+     * // Send setpoint to offboard controller PID
+     * m_motor.setSetpoint(
+     * ExampleSmartMotorController.PIDMode.kPosition,
+     * m_setpoint.position,
+     * m_feedforward.calculate(m_setpoint.velocity) / 12.0);
+     */
 
- @Override
- public void teleopPeriodic() {
- /*
-   if (m_joystick.getRawButtonPressed(2)) {
-     m_goal = new TrapezoidProfile.State(5, 0);
-   } else if (m_joystick.getRawButtonPressed(3)) {
-     m_goal = new TrapezoidProfile.State();
-   }
+    // SmartDashboard.putNumber("Elevator Rotations", m_motor.getEncoderDistance());
+    // SmartDashboard.putNumber("Target", goal);
+    // SmartDashboard.putNumber("Error", m_motor.getClosedLoopError());
 
-   // Retrieve the profiled setpoint for the next timestep. This setpoint moves
-   // toward the goal while obeying the constraints.
-   m_setpoint = m_profile.calculate(kDt, m_setpoint, m_goal);
+    SmartDashboard.putNumber("Elevator Rotations", m_motor.getEncoderDistance( ));
+    SmartDashboard.putNumber("Target", goal);
+    SmartDashboard.putNumber("Error", m_motor.getClosedLoopError( ));
+    SmartDashboard.putNumber("Kp", m_motor.getKp( ));
+    SmartDashboard.putNumber("Velocity", m_motor.getVelocity( ));
 
-   // Send setpoint to offboard controller PID
-   m_motor.setSetpoint(
-       ExampleSmartMotorController.PIDMode.kPosition,
-       m_setpoint.position,
-       m_feedforward.calculate(m_setpoint.velocity) / 12.0);
-   */
+    m_elevSim.periodic( );
 
-   // SmartDashboard.putNumber("Elevator Rotations", m_motor.getEncoderDistance());
-   // SmartDashboard.putNumber("Target", goal);
-   // SmartDashboard.putNumber("Error", m_motor.getClosedLoopError());
+    if (m_controller.getAButtonPressed( ))
+    { // if A button pressed, set voltage of 0.3
+      m_motor.set(0.3);
+      DataLogManager.log("A Button Pressed -- Voltage PercentOutput: 0.3");
+    }
 
-   SmartDashboard.putNumber("Elevator Rotations", m_motor.getEncoderDistance());
-   SmartDashboard.putNumber("Target", goal);
-   SmartDashboard.putNumber("Error", m_motor.getClosedLoopError());
-   SmartDashboard.putNumber("Kp", m_motor.getKp());
-   SmartDashboard.putNumber("Velocity", m_motor.getVelocity());
+    if (m_controller.getBButtonPressed( ))
+    { // if B button pressed, set voltage of -0.3
+      m_motor.set(-0.3);
+      DataLogManager.log("B Button Pressed -- Voltage PercentOutput: -0.3");
+    }
 
-   m_elevSim.periodic( );
-   
-   if (m_controller.getAButtonPressed()) { // if A button pressed, set voltage of 0.3
-     m_motor.set(0.3);
-     DataLogManager.log("A Button Pressed -- Voltage PercentOutput: 0.3");
-   }
-
-   if (m_controller.getBButtonPressed()) { // if B button pressed, set voltage of -0.3
-     m_motor.set(-0.3);
-     DataLogManager.log("B Button Pressed -- Voltage PercentOutput: -0.3");
-   }
- 
-   m_setpoint = m_profile.calculate(kDt, m_setpoint, m_goal);
-    m_motor.setSetpoint(
-       ExampleSmartMotorController.PIDMode.kPosition,
-       m_setpoint.position,
-       m_feedforward.calculate(m_setpoint.velocity) / 12.0); // why divide by 12?
-
-   if (m_controller.getXButtonPressed()) { // if X button pressed, set PID at setpoint of 1.0
+    if (m_controller.getXButtonPressed( ))
+    { // if X button pressed, set PID at setpoint of 1.0
      // m_motor.setSetpoint(ExampleSmartMotorController.PIDMode.kPosition, 1.0, 0);
      // DataLogManager.log("X Button Pressed -- PID Setpoint: 1.0");
-     // goal = 4096.0;
-     m_goal = new TrapezoidProfile.State(5, 0); // test this value (position value :)
-     DataLogManager.log("X Button Pressed -- Trapezoid Profile Setpoint: 1.0");
-   }
+     // goal = 4096.0;    // TODO: The whole point of putting the encoder conversions in ExampleSmartMotorController is to hide them from this "Robot.java" level
+     //                             so we won't be using this "goal = 4096.0", because m_goal will be in rotations!
+      m_goal = new TrapezoidProfile.State(3, 0); // test this value (position value :)
+      DataLogManager.log("X Button Pressed -- Trapezoid Profile Setpoint: 1.0");
+    }
 
-   if (m_controller.getYButtonPressed()) { // if Y button pressed, set PID at setpoint of 0.0
+    if (m_controller.getYButtonPressed( ))
+    { // if Y button pressed, set PID at setpoint of 0.0
      // m_motor.setSetpoint(ExampleSmartMotorController.PIDMode.kPosition, 0.0, 0);
      // DataLogManager.log("Y Button Pressed -- PID Setpoint: 0.0");
      // goal = 0.0;
-    m_goal = new TrapezoidProfile.State(0, 0); // test this value (position value :)
-     DataLogManager.log("Y Button Pressed -- Trapezoid Profile Setpoint: 0.0");
-   }
+      m_goal = new TrapezoidProfile.State(0, 0); // test this value (position value :)
+      DataLogManager.log("Y Button Pressed -- Trapezoid Profile Setpoint: 0.0");
+    }
 
-   if (m_controller.getRightBumperPressed()) { // if Right Bumper pressed, stop Motor
-     m_motor.stopMotor();
-     DataLogManager.log("Right Bumper Pressed -- Motor Stopped");
-   }
+    // TODO: Since this USES the m_goal, it should run after m_goal is set above
+    //        Also, since this runs every time, it will block all other motor commands (like set())
+    //        We only want this to run when the X or Y button has been pressed, so maybe it makes 
+    //        sense to have the X and Y buttons ENABLE this section of code through a boolean
+    //        while this mode is active. Then clear that enable flag when you want to use other 
+    //        modes
+    //  if (m_pidEnabled)
+    //    {}
+    //
+    m_setpoint = m_profile.calculate(kDt, m_setpoint, m_goal);
+    m_motor.setSetpoint(ExampleSmartMotorController.PIDMode.kPosition, m_setpoint.position,
+        m_feedforward.calculate(m_setpoint.velocity) / 12.0); // why divide by 12?
 
-   if (m_controller.getRawButtonPressed(8)) { // if menu button, invert motor direction
-     DataLogManager.log("Menu Button Pressed"); // controller is not reading the button being pressed?
-     if (m_motor.getInverted()) {
-       m_motor.setInverted(false);
-       DataLogManager.log("Motor Inverted -- False");
-     } else {
-       m_motor.setInverted(true);
-       DataLogManager.log("Motor Inverted -- True");
-     }
-   }
- }
+    if (m_controller.getRightBumperPressed( ))
+    { // if Right Bumper pressed, stop Motor
+      m_motor.stopMotor( );
+      DataLogManager.log("Right Bumper Pressed -- Motor Stopped");
+    }
+
+    if (m_controller.getRawButtonPressed(8))
+    { // if menu button, invert motor direction
+      DataLogManager.log("Menu Button Pressed"); // controller is not reading the button being pressed?
+      if (m_motor.getInverted( ))
+      {
+        m_motor.setInverted(false);
+        DataLogManager.log("Motor Inverted -- False");
+      }
+      else
+      {
+        m_motor.setInverted(true);
+        DataLogManager.log("Motor Inverted -- True");
+      }
+    }
+  }
 }
